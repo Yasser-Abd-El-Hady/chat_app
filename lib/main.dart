e26d5940e7c31580ed29fa150607b454470080be
Yasser-Abd-El-Hady <yasserabdelhady22@gmail.com>
2022-10-14 00:12:33 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:chat_app/firebase_options.dart';
-import 'package:chat_app/views/screens/auth_screens/chat_screen.dart';
-import 'package:chat_app/views/screens/auth_screens/home_screen.dart';
+import 'package:chat_app/views/screens/chat_screen.dart';
+import 'package:chat_app/views/screens/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
